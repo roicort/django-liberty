7a5f31a29b7732a5b5b35c367e80ba8e6c16e578
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2025-03-10 12:18:34 -0600

    Fix wellKnown

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -47,7 +47,7 @@ if frontend == "next":
         "OIDC_CLIENT_ID": oidc_client_id,
         "OIDC_CLIENT_SECRET": oidc_client_secret,
         "OIDC_ISSUER": "http://backend:8000/oidc",
-        "OIDC_WELL_KNOWN": "http://backend:8000/oidc/.well-known/openid-configuration/?name=provider-info",
+        "OIDC_WELL_KNOWN": "http://backend:8000/oidc/.well-known/openid-configuration",
         "NEXTAUTH_URL": "http://frontend:3000"
     }
     dir_to_remove = "frontend-nuxt"
